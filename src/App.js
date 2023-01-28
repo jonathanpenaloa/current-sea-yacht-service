af539797e9f56d1b3a720e136d9ac7e69d9714f6
jonathanpenaloa <jona_pena_91@outlook.com>
2023-01-27 19:50:26 -0800

    componet moduels added, react-app started code removed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './index.css';
 
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Yacht from "./routes/Yacht";
+ 
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>  
+      <Route to='/' element={<Home />}/>
+      <Route to='/About' element={<About />}/>
+      <Route to='/Yacht' element={<Yacht />}/>
+      <Route to='/Contact' element={<Contact />}/>
+    </Routes>
+    </>
   );
 }
 
-export default App;
---
+++ src/routes/Yacht.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Yacht() {
+    return(
+        <div>
+            <h1>Yacht Page</h1>
+        </div>
+    );
+}
