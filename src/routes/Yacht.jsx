a99f3db51538f26520fdb8bc463189d297ca7286
jonathanpenaloa <jona_pena_91@outlook.com>
2023-01-30 19:56:15 -0800

    assets added

--- src/routes/Yacht.jsx
+++ src/routes/Yacht.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
+import PagesInfo from "../components/PagesInfo/PagesInfo";
 
 export default function Yacht() {
     return(
         <div>
             <NavBar />
-            <h1>Yacht Page</h1>
+            <PagesInfo />
+            <Footer />
         </div>
     );
 }
