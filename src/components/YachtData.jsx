ddbb4e4afeaf1b278b670bdef157c69acd3157b9
jonathanpenaloa <jona_pena_91@outlook.com>
2023-01-30 19:10:59 -0800

    yacht data added

---
+++ src/components/YachtData.jsx
@@ -0,0 +1,23 @@
+import Horizon from "../assets/Horizon.jpg";
+import Sundancer from "../assets/Sundancer.jpg";
+import Talaria from "../assets/Talaria.jpg";
+
+const YachtData = [
+    {
+        imgsrc: Horizon,
+        title: "2000 Harizon 70'",
+        text: "Northern A-lure is a quality build from Horizon Yachts that boasts abundant volume and great performance."
+    },
+    {
+        imgsrc: Sundancer,
+        title: "SeaRay 260 Sundancer",
+        text: "A convertible V-berth offers cozy napping or overnighting, while a spacious cockpit with ample seating offers endless views of the horizon, the waterline, or the conversation at hand."
+    },
+    {
+        imgsrc: Talaria,
+        title: "Hinckley Taliria 29' Runabout",
+        text: "Easy handling and looks that are every bit as impressive"
+    }
+]
+
+export default YachtData;
