864667be5342ecd2970b3d89cda142008e38a812
jonathanpenaloa <jona_pena_91@outlook.com>
2023-01-31 23:50:52 -0800

    Contacted info added

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import dennisImg from "../../assets/placeholder.jpg";
+// import dennisImg from "../../assets/placeholder.jpg";
 
 
 import "./AboutUs.css";
@@ -12,12 +12,16 @@ export default function AboutUs() {
                 <h1>Dennis Oswalt</h1>
                 <p>425-281-6785</p>
                 <p>DOswalt@csyachting.com</p>
+                <br/>
+                <h1>Marvin Nielson</h1>
+                <p>206-434-9035</p>
+                <p>MNielson@csyachting.com</p>
             </div>
 
             <div className="right">
                 <div className="img-container">
                     <div className="img-stack top">
-                        <img className="img" src={dennisImg} alt="img" />
+                        {/* <img className="img" src={dennisImg} alt="img" /> */}
                     </div>
                 </div>
             </div>
